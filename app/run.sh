680082e08bedee44465e4af13e2f21e68bb11598
ntatrishvili <nia.tatrishvili@gmail.com>
2023-07-18 22:02:25 +0200

    fix: remove cloud-init commands from run.sh

--- app/run.sh
+++ app/run.sh
@@ -2,8 +2,4 @@
 
 wget https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar
 
-apt update
-apt upgrade
-apt install openjdk-18-jre -y
-
 java -jar ./server.jar
